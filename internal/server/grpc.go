78251dadb44a95a89ffe6e4c8152a40196e2cb13
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-03-17 14:29:47 +0530

    refc:Modify grpc.go and update test

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -11,7 +11,11 @@ type IngestServer struct {
 	pb.UnimplementedChessIngestServiceServer
 }
 
-func (is IngestServer) RecordMove(ctx context.Context, m *pb.Move) (*pb.MoveResponse, error) {
+func (is *IngestServer) RecordMove(ctx context.Context, m *pb.Move) (*pb.MoveResponse, error) {
 	log.Print(m.String())
 	return &pb.MoveResponse{Success: true, Msg: "Success"}, nil
+}
+
+func NewIngestServer() *IngestServer {
+	return &IngestServer{}
 }
