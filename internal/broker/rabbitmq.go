41049c63f73e137705422aa20786c8c45aae3d39
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-03-19 14:24:54 +0530

    feat: Add real RabbitMQ publisher

---
+++ internal/broker/rabbitmq.go
@@ -0,0 +1,47 @@
+package broker
+
+import (
+	"context"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQPublisher struct {
+	ch *amqp091.Channel
+}
+
+func (rp *RabbitMQPublisher) PublishMove(ctx context.Context, move []byte) error {
+	err := rp.ch.PublishWithContext(ctx,
+		"chess_broadcast",
+		"",
+		false,
+		false,
+		amqp091.Publishing{ContentType: "text/plain", Body: move},
+	)
+	return err
+}
+
+func NewRabbitMQPublisher(connURL string) (*RabbitMQPublisher, error) {
+	rp := &RabbitMQPublisher{}
+	conn, err := amqp091.Dial(connURL)
+	if err != nil {
+		return nil, err
+	}
+	rp.ch, err = conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	err = rp.ch.ExchangeDeclare(
+		"chess_broadcast",
+		"fanout",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return rp, nil
+}
