8d5d215188679f4c8f622ee6df9d93f62e479561
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-03-22 23:25:13 +0530

    feat: Implement websocket hub and client

---
+++ internal/websocket/client.go
@@ -0,0 +1,73 @@
+package websocket
+
+import (
+	"log/slog"
+	"net/http"
+
+	ws "github.com/gorilla/websocket"
+)
+
+var upgrader = ws.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+type Client struct {
+	hub  *Hub
+	conn *ws.Conn
+	send chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		err := c.conn.Close()
+		if err != nil {
+			slog.Error("Error closing connection", "Error", err)
+		}
+		c.hub.unregister <- c
+	}()
+	for {
+		_, _, err := c.conn.ReadMessage()
+		if err != nil {
+			if ws.IsUnexpectedCloseError(err, ws.CloseGoingAway, ws.CloseAbnormalClosure) {
+				slog.Error("Connection closed", "Error", err)
+			}
+			break
+		}
+	}
+}
+
+func (c *Client) writePump() {
+	defer func() {
+		err := c.conn.Close()
+		if err != nil {
+			slog.Error("Error closing connection", "Error", err)
+		}
+		c.hub.unregister <- c
+	}()
+	for {
+		msg := <-c.send
+		err := c.conn.WriteMessage(ws.TextMessage, msg)
+		if err != nil {
+			slog.Error("Error sending message", "Error", err)
+			break
+		}
+	}
+}
+
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	client := Client{
+		hub:  hub,
+		conn: conn,
+		send: make(chan []byte),
+	}
+	client.hub.register <- &client
+	go client.writePump()
+	go client.readPump()
+}
---
+++ internal/websocket/hub.go
@@ -0,0 +1,43 @@
+package websocket
+
+import (
+	"sync"
+)
+
+type Hub struct {
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+	clients    map[*Client]bool
+	sync.RWMutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (hub *Hub) Run() {
+	for {
+		select {
+		case client := <-hub.register:
+			hub.Lock()
+			hub.clients[client] = true
+			hub.Unlock()
+		case client := <-hub.unregister:
+			hub.Lock()
+			delete(hub.clients, client)
+			hub.Unlock()
+		case msg := <-hub.broadcast:
+			hub.RLock()
+			for client := range hub.clients {
+				client.send <- msg
+			}
+			hub.RUnlock()
+		}
+	}
+}
